34b00f90a27daa3a1b2d76b1bf1ac2bcb16cbf99
Nerdy Beardo <nerdybeardo@gmail.com>
2014-03-29 12:25:29 -0400

    add gulp build to azure deployment

--- nodescript/deploy.cmd
+++ nodescript/deploy.cmd
@@ -88,34 +88,23 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-echo 1. Select node version
-call :SelectNodeVersion
-
-pushd src\web
-echo 2. Install npm packages
-IF EXIST "package.json" (
-  call :ExecuteCmd !NPM_CMD! install --production
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-
-echo "Execute Gulp"
-IF EXIST "Gulpfile.js" (
-  call .\node_modules\.bin\gulp build
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-popd
-
-echo 3. KuduSync
+:: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
-echo 5. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\src\web\dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+:: 2. Select node version
+call :SelectNodeVersion
+
+:: 3. Install npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
+  popd
 )
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Post deployment stub
@@ -147,4 +136,3 @@ exit /b 1
 :end
 endlocal
 echo Finished successfully.
-
--- src/TodoSample.Api/Controllers/NotificationController.cs
+++ src/TodoSample.Api/Controllers/NotificationController.cs
@@ -4,14 +4,15 @@ using TodoSample.Api.Hubs;
 using TodoSample.Data.Entities;
 using TodoSample.Data.Messages;
 
-namespace api.Controllers {
-    public class NotificationController : ApiController {
+namespace api.Controllers
+{
+    public class NotificationController : ApiController
+    {
         // POST api/notification
         public void Post([FromBody]PointTally tally)
         {
             var todoHub = GlobalHost.ConnectionManager.GetHubContext<TodosHub>();
-	    todoHub.Clients.All.OnTotalsUpdated(tally);
+            todoHub.Clients.All.OnTotalsUpdated(tally);
         }
-
     }
 }
